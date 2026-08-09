9ab28ab9af8ebd6bf56f749f0f032c378f7ec863
Neil Thomas Mathew <neilthomasmathew123@gmail.com>
2026-08-09 14:00:51 +0530

    Scaffold Gradle root project and wrapper

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+rootProject.name = "proximity-os"
+
+include(":shared")
+include(":androidApp")
