9ecfef84ccc3d8062dfb643032fb9e3b8560ebf2
Neil Thomas Mathew <neilthomasmathew123@gmail.com>
2026-08-09 14:02:41 +0530

    Scaffold androidApp module manifest and build config

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+android {
+    namespace = "os.proximity.android"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "os.proximity.android"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.core.ktx)
+    implementation(compose.material3)
+    implementation(compose.ui)
+    implementation(compose.runtime)
+    debugImplementation(compose.uiTooling)
+}
